6a771df880f195a474791cba38a04f5a0bc47b85
erwanngat <ewnran69@yahoo.fr>
2024-11-07 10:48:28 +0100

    add app2 + location

---
+++ App2/weatherApp/App.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import { View, Text, Button } from "react-native";
+import * as Location from "expo-location";
+
+export default function App() {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    const getLocationPermission = async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    };
+
+    getLocationPermission();
+  }, []);
+
+  let text = "Waiting..";
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = `Latitude: ${location.coords.latitude}\nLongitude: ${location.coords.longitude}`;
+  }
+
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>{text}</Text>
+    </View>
+  );
+}
